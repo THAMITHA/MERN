f0643733048762cce7eb13e623c0e2bbc009415c
THAMITHA S <THAMITHA.S@ibm.com>
2023-12-28 12:35:31 +0530

    add superheroes

--- 2.3 NPM/index.js
+++ 2.3 NPM/index.js
@@ -1,5 +1,9 @@
-var generateName = require('sillyname');
-var sillyName = generateName();
+//var generateName = require('sillyname');
 
+import generateName from "sillyname";
+import superheroes from "superheroes";
+var sillyName = generateName();
+var name = superheroes.random();
 
 console.log(`my name is ${sillyName}.`);
+console.log(`i am ${name}.`);
