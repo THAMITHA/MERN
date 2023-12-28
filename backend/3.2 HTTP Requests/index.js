3d1efbdf66c941e82d237f4a62daa00c568318a9
THAMITHA S <THAMITHA.S@ibm.com>
2023-12-29 00:38:42 +0530

    add http request

--- backend/3.2 HTTP Requests/index.js
+++ backend/3.2 HTTP Requests/index.js
@@ -3,9 +3,16 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res)=>{
-    res.send("Hello World!");
+    res.send("<h1>Hello!</h1>");
 });
 
+app.get("/about", (req, res)=>{
+    res.send("<h1>About Me!!!</h1><p>My name is Thamitha</p>");
+})
+
+app.get("/contact", (req, res)=>{
+    res.send("<h1>Contact Me</h1><p>Phone: +214356766532</p>")
+})
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}`);
 });
