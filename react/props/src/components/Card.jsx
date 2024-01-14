8ecb373cfa9c82417ff6cc24b017707c883cdfb4
THAMITHA S <THAMITHA.S@ibm.com>
2024-01-14 23:30:03 +0530

    add contactList

--- react/props/src/components/Card.jsx
+++ react/props/src/components/Card.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Detail from "./Detail";
+import Avatar from "./Avatar";
 
 function Card(props) {
   return (
@@ -6,11 +8,11 @@ function Card(props) {
       <div className="card">
         <div className="top">
           <h2 className="name">{props.name}</h2>
-          <img className="circle-img" src={props.img} alt="avatar_img" />
+          <Avatar img= {props.img}/>
         </div>
         <div className="bottom">
-          <p className="info">{props.phno}</p>
-          <p className="info">{props.email}</p>
+          <Detail detailInfo={props.phone}/>
+          <Detail detailInfo={props.email}/>
         </div>
       </div>
       
